2152c445c85550d1ab6ae75b02ccb039f6f2ae9f
Julio Mena Pinto <julio.mena@alnus.cl>
2020-05-28 16:51:45 -0400

    chore(types): added types

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -3,56 +3,44 @@
  */
 export interface BaseProvider {
   /**
-   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
+   * Método que autentifica contra el proveedor el usuario con privilegios
+   * para crear una reunión.
    *
-   * Formatea la pregunta leída desde la base de datos.
-   * @param {boolean} param - Primer parametro.
+   * @param {LoginProps} props - Parámetros para el Login del Proveedor CEV.
    */
-  login(param: boolean): void
+  login: (props: LoginProps) => RequestStatus
 
   /**
-   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
-   *
-   * Formatea la pregunta leída desde la base de datos.
-   * @param {boolean} param - Primer parametro.
+   * Método que permite añadir un participante a la reunión.
+   * @param {Participant} participant - Instancia de participante a añadir.
    */
-  createParticipant(param: boolean): void
+  createParticipant: (participant: Participant) => void
   /**
-   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
-   *
-   * Formatea la pregunta leída desde la base de datos.
-   * @param {boolean} param - Primer parametro.
+   * Método que permite editar un participante de la reunión.
+   * @param {Participant} participant - Instancia de participante a editar.
    */
-  editParticipant(param: boolean): void
+  editParticipant: (participant: Participant) => void
   /**
-   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
-   *
-   * Formatea la pregunta leída desde la base de datos.
-   * @param {boolean} param - Primer parametro.
+   * Método que permite eliminar un participante de la reunión.
+   * @param {Participant} participant - Instancia de participante a eliminar.
    */
-  deleteParticipant(param: boolean): void
+  deleteParticipant: (participant: Participant) => void
 
   /**
-   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
-   *
-   * Formatea la pregunta leída desde la base de datos.
-   * @param {boolean} param - Primer parametro.
+   * Método que permite crear una reunión.
+   * @param {Meeting} meeting - Instancia de meeting a crear.
    */
-  createMeeting(param: boolean): void
+  createMeeting: (meeting: Meeting) => void
   /**
-   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
-   *
-   * Formatea la pregunta leída desde la base de datos.
-   * @param {boolean} param - Primer parametro.
+   * Método que permite editar una reunión.
+   * @param {Meeting} meeting - Instancia de meeting a editar.
    */
-  editMeeting(param: boolean): void
+  editMeeting: (meeting: Meeting) => void
   /**
-   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
-   *
-   * Formatea la pregunta leída desde la base de datos.
-   * @param {boolean} param - Primer parametro.
+   * Método que permite eliminar una reunión.
+   * @param {Meeting} meeting - Instancia de meeting a eliminar.
    */
-  deleteMeeting(param: boolean): void
+  deleteMeeting: (meeting: Meeting) => void
 
   /**
    * @todo Complementar parametros y respuestas según funcionamiento actual V6.
@@ -68,4 +56,88 @@ export interface BaseProvider {
    * @param {boolean} param - Primer parametro.
    */
   goMeetingTeacher(param: boolean): void
+}
+
+/**
+ * Parámetros de la función login del Proveedor CEV.
+ */
+export type LoginProps = {
+  /**
+   * URL API.
+   */
+  url: string
+  /**
+   * Usuario a loguear en el Proveedor CEV.
+   */
+  username?: string
+  /**
+   * Clave del usuario a loguear en el Proveedor CEV.
+   */
+  password?: string
+  /**
+   * Token del Proveedor CEV.
+   */
+  token?: string
+}
+
+export type Meeting = {
+  /**
+   * URL de la reunión.
+   */
+  url: string
+}
+
+/**
+ * Instancia de Participante a la reunión del Proveedor CEV.
+ */
+export type Participant = {
+  /**
+   * Nombre de usuario al proveedor CEV.
+   */
+  username: string
+  /**
+   * Contraseña del usuario al proveedor CEV.
+   */
+  password: string
+  /**
+   * Nombre del participante.
+   */
+  firstName?: string
+  /**
+   * Apellido del participante.
+   */
+  lastName?: string
+}
+
+/**
+ * Objeto de respuesta para todas las peticiones.
+ */
+export interface RequestStatus {
+  /**
+   * ### Estado de la petición
+   * - __TRUE__: Si es exitosa y no se capturó ningún error
+   * - __FALSE__: Si se capturó algún error
+   */
+  success: boolean
+
+  /**
+   * Nombre del estado.
+   */
+  name: string
+
+  /**
+   * Detalle de la Petición. En caso de error se muestra el mensaje de error de la la excepción.
+   */
+  detail: string
+
+  /**
+   * Código de la petición.
+   */
+  code: number
+
+  /**
+   * Error que dispara el resolver.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  err?: any
 }
