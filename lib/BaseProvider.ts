7be366cc18d958f009ac37feb87b0782411a14fd
Julio Mena Pinto <julio.mena@alnus.cl>
2020-06-03 12:55:41 -0400

    feat(base-provider): refactor to abstract class

---
+++ lib/BaseProvider.ts
@@ -0,0 +1,72 @@
+import { LoginProps, RequestStatus, Participant, Meeting } from './types'
+
+/**
+ * **Módulo** de Proveedor CEV.
+ */
+export abstract class BaseProvider {
+  /**
+   * Método que autentifica contra el proveedor el usuario con privilegios
+   * para crear una reunión.
+   *
+   * @param {LoginProps} props - Parámetros para el Login del Proveedor CEV.
+   */
+  abstract login (props: LoginProps): Promise<RequestStatus>
+
+  /**
+   * Método que permite añadir un participante a la reunión.
+   * @param {string} token - Token para acceder a la API Proveedor CEV.
+   * @param {Participant} participant - Instancia de participante a añadir.
+   */
+  // abstract createParticipant(token: string, participant: Participant): void
+  /**
+   * Método que permite editar un participante de la reunión.
+   * @param {string} token - Token para acceder a la API Proveedor CEV.
+   * @param {Participant} participant - Instancia de participante a editar.
+   */
+  // abstract editParticipant(token: string, participant: Participant): void
+  /**
+   * Método que permite eliminar un participante de la reunión.
+   * @param {string} token - Token para acceder a la API Proveedor CEV.
+   * @param {Participant} participant - Instancia de participante a eliminar.
+   */
+  // abstract deleteParticipant(token: string, participant: Participant): void
+
+  /**
+   * Método que permite crear una reunión.
+   * @param {Meeting} meeting - Instancia de meeting a crear.
+   * @param {string} [token] - Token para acceder a la API Proveedor CEV.
+   */
+  abstract createMeeting (
+    meeting: Meeting,
+    token: string
+  ): Promise<RequestStatus>
+  /**
+   * Método que permite editar una reunión.
+   * @param {string} token - Token para acceder a la API Proveedor CEV.
+   * @param {Meeting} meeting - Instancia de meeting a editar.
+   */
+  // abstract editMeeting(token: string, meeting: Meeting): void
+  /**
+   * Método que permite eliminar una reunión.
+   * @param {string} token - Token para acceder a la API Proveedor CEV.
+   * @param {Meeting} meeting - Instancia de meeting a eliminar.
+   */
+  // abstract deleteMeeting(token: string, meeting: Meeting): void
+
+  /**
+   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
+   *
+   * Formatea la pregunta leída desde la base de datos.
+   * @param {string} token - Token para acceder a la API Proveedor CEV.
+   * @param {boolean} param - Primer parametro.
+   */
+  // abstract goMeeting(token: string, param: boolean): void
+  /**
+   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
+   *
+   * Formatea la pregunta leída desde la base de datos.
+   * @param {string} token - Token para acceder a la API Proveedor CEV.
+   * @param {boolean} param - Primer parametro.
+   */
+  // abstract goMeetingTeacher(token: string, param: boolean): void
+}
--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,79 +1,11 @@
 /**
- * **Módulo** de Proveedor CEV.
- */
-export interface BaseProvider {
-  /**
-   * Método que autentifica contra el proveedor el usuario con privilegios
-   * para crear una reunión.
-   *
-   * @param {LoginProps} props - Parámetros para el Login del Proveedor CEV.
-   */
-  login: (props: LoginProps) => Promise<RequestStatus>
-
-  /**
-   * Método que permite añadir un participante a la reunión.
-   * @param {string} token - Token para acceder a la API Proveedor CEV.
-   * @param {Participant} participant - Instancia de participante a añadir.
-   */
-  createParticipant: (token: string, participant: Participant) => void
-  /**
-   * Método que permite editar un participante de la reunión.
-   * @param {string} token - Token para acceder a la API Proveedor CEV.
-   * @param {Participant} participant - Instancia de participante a editar.
-   */
-  editParticipant: (token: string, participant: Participant) => void
-  /**
-   * Método que permite eliminar un participante de la reunión.
-   * @param {string} token - Token para acceder a la API Proveedor CEV.
-   * @param {Participant} participant - Instancia de participante a eliminar.
-   */
-  deleteParticipant: (token: string, participant: Participant) => void
-
-  /**
-   * Método que permite crear una reunión.
-   * @param {string} token - Token para acceder a la API Proveedor CEV.
-   * @param {Meeting} meeting - Instancia de meeting a crear.
-   */
-  createMeeting: (token: string, meeting: Meeting) => Promise<RequestStatus>
-  /**
-   * Método que permite editar una reunión.
-   * @param {string} token - Token para acceder a la API Proveedor CEV.
-   * @param {Meeting} meeting - Instancia de meeting a editar.
-   */
-  editMeeting: (token: string, meeting: Meeting) => void
-  /**
-   * Método que permite eliminar una reunión.
-   * @param {string} token - Token para acceder a la API Proveedor CEV.
-   * @param {Meeting} meeting - Instancia de meeting a eliminar.
-   */
-  deleteMeeting: (token: string, meeting: Meeting) => void
-
-  /**
-   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
-   *
-   * Formatea la pregunta leída desde la base de datos.
-   * @param {string} token - Token para acceder a la API Proveedor CEV.
-   * @param {boolean} param - Primer parametro.
-   */
-  goMeeting: (token: string, param: boolean) => void
-  /**
-   * @todo Complementar parametros y respuestas según funcionamiento actual V6.
-   *
-   * Formatea la pregunta leída desde la base de datos.
-   * @param {string} token - Token para acceder a la API Proveedor CEV.
-   * @param {boolean} param - Primer parametro.
-   */
-  goMeetingTeacher: (token: string, param: boolean) => void
-}
-
-/**
  * Parámetros de la función login del Proveedor CEV.
  */
 export type LoginProps = {
   /**
    * URL API.
    */
-  url: string
+  url?: string
   /**
    * Usuario a loguear en el Proveedor CEV.
    */
@@ -162,10 +94,10 @@ export interface RequestStatus {
   /**
    * Error que dispara el resolver.
    */
-  err?: unknown
+  err?: any
 
   /**
    * Data asociada a request.
    */
-  data?: unknown
+  data?: any
 }
